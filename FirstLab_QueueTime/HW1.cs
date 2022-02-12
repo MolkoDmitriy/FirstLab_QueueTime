b5f17b5bfec414732ba5151a597b616f79b00f80
Дмитрий Молько <molko_dmitriy@mail.ru>
2022-02-12 13:32:27 +0700

    Сделал лабораторную 1

--- FirstLab_QueueTime/HW1.cs
+++ FirstLab_QueueTime/HW1.cs
@@ -10,7 +10,36 @@ namespace FirstLab_QueueTime
     {
         public static long QueueTime(int[] customers, int n)
         {
-            return 0;
+            int[] cashbox = new int[n];
+            
+            int time = 0;
+            do
+            {
+                int sum = 0;
+                for (int i = 0; i < customers.Length; i++) sum += customers[i];
+                if (sum == 0) break;
+                for (int i = 0; i < n; i++) cashbox[i] = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (customers[i] == 0) cashbox[i] = 1;
+                }
+                for (int i = 0; i < cashbox.Length; i++)
+                {
+                    if (cashbox[i] == 1)
+                    {
+                        customers[i] = customers[n];
+                        for (int j= n; j < customers.Length-1; j++)
+                        {
+                            customers[j] = customers[j + 1];
+                        }
+                        customers[customers.Length - 1] = 0;
+                    }
+                    if (customers[i] != 0)
+                        customers[i]--;
+                }
+                time++;
+            } while (true);
+            return time;
         }
 
     }
--- FirstLab_QueueTime/Program.cs
+++ FirstLab_QueueTime/Program.cs
@@ -6,8 +6,12 @@ namespace FirstLab_QueueTime
     {
         static void Main(string[] args)
         {
-            int[] num1 = { 1, 2, 3 };
-            HW1.QueueTime(num1,4);
+            int[] num1 = { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(num1,1));
+            int[] num2 = { 10, 2, 3, 3 };
+            Console.WriteLine(HW1.QueueTime(num2,2)); 
+            int[] num3 = { 2, 3, 10 };
+            Console.WriteLine(HW1.QueueTime(num3,2));
         }
     }
 }
