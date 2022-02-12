5c151a15b4410de9bf8be6d8a28edec00402e4ba
Дмитрий Молько <molko_dmitriy@mail.ru>
2022-02-12 12:29:54 +0700

    Добавьте файлы проекта.

---
+++ FirstLab_QueueTime/HW1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLab_QueueTime
+{
+    class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ FirstLab_QueueTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstLab_QueueTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 1, 2, 3 };
+            HW1.QueueTime(num1,4);
+        }
+    }
+}
